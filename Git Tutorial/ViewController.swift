7b79c3d96edf32bc76c56852e2628bb103a40257
Prashant Gaikwad <prashant.gaikwad@leftrightmind.com>
2018-04-06 10:51:30 +0530

    Added  message string constant which prints when the view loads

--- Git Tutorial/ViewController.swift
+++ Git Tutorial/ViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    let message = "Hi, How are you ?"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print(message)
     }
 
     override func didReceiveMemoryWarning() {
